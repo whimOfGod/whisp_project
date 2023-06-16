47b1e19f246e77c4ff4641f61d8598402c8f3a19
aaron kolingba <78544822+whimOfGod@users.noreply.github.com>
2023-06-16 10:03:31 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+<?php include 'template/header.php' ?>    
+        <div class="formContent">
+            <form action="">
+                <h2> formulaire </h2>
+                    <label for="">Pseudo</label> 
+                    <input class="myLoginInput" type="text" placeholder="  your pseudo " > 
+                    <label for="">mot de passe</label>
+                    <input class="myLoginInput" type="password" name="" id="" placeholder="  ********  ">
+            </form>
+        </div>
+
+    <script src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>    
+</body>
+</html>
